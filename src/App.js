abc86f3825bd9f5b6c7c4c5ff58f0aa266a7ad0d
Ankit rahi <120669540+Ankit9179@users.noreply.github.com>
2023-09-02 17:30:22 +0530

    completed home page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 function App() {
   return (
     <>
       <div className="min-h-screen bg-center bg-cover p-4 bg-[url('https://img.freepik.com/free-photo/abstract-textured-backgound_1258-30436.jpg?w=900&t=st=1693628859~exp=1693629459~hmac=89a5efd11b54d54de48a747da22a75871f748571615af8ba46ef15e1973baf1f')]">
         <Navbar />
+        <Home />
       </div>
     </>
   );
