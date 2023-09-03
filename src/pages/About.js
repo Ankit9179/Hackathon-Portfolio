984e026616ff0591d94165c7dc788a8c407fc4a7
Ankit rahi <120669540+Ankit9179@users.noreply.github.com>
2023-09-03 07:01:47 +0530

    about page

---
+++ src/pages/About.js
@@ -0,0 +1,37 @@
+import React from "react";
+import aboutpng from "../images/home.png";
+
+const About = () => {
+  return (
+    <div>
+      <>
+        <div className="block md:flex border-2 p-12 w-full justify-center">
+          <div className="right border-2 w-[50">
+            <img className="w-[500px]" src={aboutpng} alt="png" />
+          </div>
+          <div className="w-[100%] md:border-2 text-white text-center  flex-col my-auto">
+            <h1 className="text-5xl font-bold mb-4">About me</h1>
+            <p className="mb-4 leading-normal">
+              lorem5 lorem3 Lorem Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Optio lorem5 , delectus? ipsum dolor sit amet
+              consectetur adipisicing lorem3 Lorem Lorem ipsum dolor sit amet
+              consectetur adipisicing elit. Optio lorem5 , delectus? ipsum dolor
+              sit amet consectetur adipisicing lorem3 Lorem Lorem ipsum dolor
+              sit amet consectetur adipisicing elit. Optio lorem5 , delectus?
+              ipsum dolor sit amet consectetur adipisicing elit. Dicta quaerat
+              maiores nisi quod similique. Nulla. consectetur adipisicing lorem3
+              Lorem Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Optio lorem5 , delectus? ipsum dolor sit amet consectetur
+              adipisicing lorem3 Lorem Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Optio lorem5 , delectus? ipsum dolor sit amet
+              consectetur adipisicing elit. Dicta quaerat maiores nisi quod
+              similique. Nulla.
+            </p>
+          </div>
+        </div>
+      </>
+    </div>
+  );
+};
+
+export default About;
